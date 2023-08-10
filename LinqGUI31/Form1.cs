9d3fc2f56a8ba82bb402df1ed9e0074c40620b83
sinheung kim <s2120@e-mirim.hs.kr>
2023-08-10 09:55:22 +0900

    Linq를 사용하여 데이터 추출, 정렬을 하여 보여주도록 하는 버튼 로직 구현

--- LinqGUI31/Form1.cs
+++ LinqGUI31/Form1.cs
@@ -12,6 +12,14 @@ namespace LinqGUI31
 {
     public partial class Form1 : Form
     {
+        static private List<Food> foodList = new List<Food>
+        {
+            new Food() {Name="짜장면", Price=8000},
+            new Food() {Name="햄버거", Price=9000},
+            new Food() {Name="탕수육", Price=15000},
+            new Food() {Name="껌", Price=1000},
+            new Food() {Name="탕후루", Price=2000}
+        };
         public Form1()
         {
             InitializeComponent();
@@ -24,17 +32,25 @@ namespace LinqGUI31
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            // list의 내용을 DataGridSource에 넣어줘서 모두 출력되게 함
+            foodBindingSource.DataSource = foodList;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            // 두번째 버튼의 클릭 이벤트 핸들러 메서드를 만들고 Linq를 사용하여 1500원 이상의 가격을 가진 음식을 비싼 순서대로 나오게 함
+            foodBindingSource.DataSource = (from item in foodList
+                                            where item.Price >= 1500
+                                            orderby item.Price descending
+                                            select item).ToList();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            // 이를 내림차순으로 나타나게 함
+            foodBindingSource.DataSource = (from item in foodList
+                                            orderby item.Name descending
+                                            select item).ToList();
         }
     }
 }
